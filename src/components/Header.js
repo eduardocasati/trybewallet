3e88192a9041e1adfe475b83dc9cfdeeb038a053
Eduardo Casati <escasati@gmail.com>
2023-04-13 01:51:55 -0400

    Resolve Requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,52 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import '../style/Header.css';
+
+import logo from '../assets/logo.svg';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <div className="header">
+        <div className="header__container">
+          <div className="header__logo">
+            <img src={ logo } alt="Money with wings" />
+            <h1>Trybewallet</h1>
+          </div>
+          <div className="header__info">
+            <span className="header__user" data-testid="email-field">
+              { email }
+            </span>
+            <span>
+              Total de Despesas:
+              {' '}
+              <span className="header__total" data-testid="total-field">
+                0
+              </span>
+              {' '}
+              <span
+                className="header__total"
+                data-testid="header-currency-field"
+              >
+                BRL
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import Header from '../components/Header';
+
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <>
+        <Header />
+        <div>
+          a
+        </div>
+      </>
+    );
   }
 }
 
