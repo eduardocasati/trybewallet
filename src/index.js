e652bce325daec6055c1e8d9c0e678795ea6f9f5
Eduardo Casati <escasati@gmail.com>
2023-04-28 18:15:05 -0400

    Remove arquivos desnecessários para o deploy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import store from './redux/store';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
@@ -16,8 +15,3 @@ ReactDOM
       </BrowserRouter>
     </Provider>,
   );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
